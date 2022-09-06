377b07aece090c900d11a1ac8cd87968931341f0
Rex <work132kimo@gmail.com>
2022-09-06 11:23:57 +0800

    change to  hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, Routes, } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Todolist from './todolist';
 import Login from './login.js';
 import Signup from './signup.js';
